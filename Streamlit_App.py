931c1a9d62d3c6b87b5e427e7f4e8ace513f3ee6
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 08:02:12 +0100

    Refactor Tawk.to chat widget integration
    Updated Tawk.to chat widget to use a fixed position container and adjusted height and scrolling.

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -561,27 +561,34 @@ if st.sidebar.button("🗑 Clear Chat", use_container_width=True):
 
 
 
-
 import streamlit as st
 
 chat_widget = """
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
+<!-- Fixed-position container -->
+<div id="tawk-container" style="
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    z-index: 9999;">
+    
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
+</div>
 """
 
 # Embed the widget in Streamlit
-st.components.v1.html(chat_widget, height=600)
+st.components.v1.html(chat_widget, height=0, scrolling=False)
 
 
 
