18f9ba4f33262a7d1a3dc14ea1f49e6f5b54fed9
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 08:52:41 +0100

    Fix Tawk.to script integration in Streamlit app

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -25,8 +25,8 @@ tawk_widget = """
 <!--Start of Tawk.to Script--> <script type="text/javascript"> var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date(); 
 (function () { var s1 = document.createElement("script"); var s0 = document.getElementsByTagName("script")[0]; s1.async = true; 
 s1.src = 'https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us'; s1.charset = 'UTF-8'; s1.setAttribute('crossorigin', '*');
-s0.parentNode.insertBefore(s1, s0); })(); </script> <!--End of Tawk.to Script--> 
-""" components.html(tawk_widget, height=50)
+s0.parentNode.insertBefore(s1, s0); })(); </script> <!--End of Tawk.to Script--> """ 
+components.html(tawk_widget, height=50)
 
 
 
