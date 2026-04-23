e1c3d0361447bb65f576cb3be285d773177563be
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 16:57:29 +0100

    Clean up Streamlit_App.py by removing unused code
    Removed redundant imports and chat widget code from Streamlit app.

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -4,14 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
 
-# Assume df is your DataFrame
-df = pd.read_csv("Analysis_Ready_DS_jobs.csv")
-import streamlit as st
-import streamlit.components.v1 as components
 
-
-
-import streamlit as st
 components.html("""
 <div style="text-align:center;">
 
@@ -23,28 +16,10 @@ function changeText(){
 """, height=0)
 
 
+# Assume df is your DataFrame
+df = pd.read_csv("Analysis_Ready_DS_jobs.csv")
 
-# ---------------- PAGE CONFIG ----------------
-import streamlit as st
 
-chat_widget = """
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-"""
-
-# Embed the widget in Streamlit
-st.components.v1.html(chat_widget, height=200)
 
 
 
