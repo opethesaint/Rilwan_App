aa7cb210a89ece21d1947167d381344e8edfb7f1
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 16:58:46 +0100

    Integrate Tawk.to chat widget into Streamlit app
    Added a chat widget using Tawk.to for user interaction.

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -20,7 +20,26 @@ function changeText(){
 df = pd.read_csv("Analysis_Ready_DS_jobs.csv")
 
 
+import streamlit as st
 
+chat_widget = """
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+"""
+
+# Embed the widget in Streamlit
+st.components.v1.html(chat_widget, height=600)
 
 
 
