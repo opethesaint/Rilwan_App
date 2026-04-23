9ebe719eaa21bc1088c048983583d09cf493fb16
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 16:13:40 +0100

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -12,36 +12,35 @@ import streamlit as st
 import streamlit as st
 import streamlit.components.v1 as components
 
-# ---------------- PAGE CONFIG ----------------
-st.set_page_config(layout="wide")
+import streamlit.components.v1 as components
 
-# ---------------- YOUR APP CONTENT ----------------
-
-
-# ---------------- TAWK CHAT WIDGET ----------------
 TAWK_SCRIPT = """
-<!--Start of Tawk.to Script-->
+<style>
+#tawk-position {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    z-index: 9999;
+}
+</style>
+
+<div id="tawk-position"></div>
+
 <script type="text/javascript">
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 
 (function() {
     var s1 = document.createElement("script");
-    var s0 = document.getElementsByTagName("script")[0];
-
     s1.async = true;
     s1.src = 'https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
-
-    s0.parentNode.insertBefore(s1, s0);
+    document.body.appendChild(s1);
 })();
 </script>
-<!--End of Tawk.to Script-->
 """
 
-# Hidden container (important)
-components.html(TAWK_SCRIPT, height=100, width=100, bottom=20,
-right=20)
+components.html(TAWK_SCRIPT, height=0, width=0)
 
 
 import streamlit as st
