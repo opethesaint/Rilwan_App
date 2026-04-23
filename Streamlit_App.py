492834cfc37157f12d26c222694c9c3c03d0e0b0
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 16:44:58 +0100

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -11,27 +11,25 @@ import streamlit.components.v1 as components
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Use a height of 0 so it doesn't create a blank space in your app
-# The script will still execute and create the floating bubble
-components.html(
-    """
-    <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
+import streamlit as st
+
+# This "tricks" the browser into executing the script by 
+# injecting it into the HTML head via a hidden element.
+st.markdown(
+    f"""
+    <div style="display:none;">
+        <img src="x" onerror="(function(){{
+            var s1=document.createElement('script');
+            s1.async=true;
+            s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            document.head.appendChild(s1);
+        }})();">
+    </div>
     """,
-    height=100,
-    width=0,
+    unsafe_allow_html=True
 )
-
-
 
 
 
