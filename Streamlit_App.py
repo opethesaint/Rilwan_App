849d3a8abd8b31b8ee428a94f43203fd03bf06c4
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 16:46:33 +0100

    Update Tawk.to chat widget integration method
    Replaced the previous method of injecting a script with a new implementation for Tawk.to chat widget integration.

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -13,24 +13,31 @@ import streamlit.components.v1 as components
 
 import streamlit as st
 
-# This "tricks" the browser into executing the script by 
-# injecting it into the HTML head via a hidden element.
-st.markdown(
-    f"""
-    <div style="display:none;">
-        <img src="x" onerror="(function(){{
-            var s1=document.createElement('script');
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Define the script
+TAWK_SCRIPT = """
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
             s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
-            document.head.appendChild(s1);
-        }})();">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+"""
 
+# Render the script in a hidden component
+# We use height=0 and width=0 to ensure it doesn't move your layout
+components.html(TAWK_SCRIPT, height=0, width=0)
+
+# The rest of your app code goes here
+st.title("My Streamlit App")
+st.write("The chat widget should appear in the bottom right corner.")
 
 
 
