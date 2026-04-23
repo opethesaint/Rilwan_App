51bc6850c277c7567a52a03638c29be2744b70f6
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 16:43:48 +0100

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -8,13 +8,28 @@ import streamlit.components.v1 as components
 df = pd.read_csv("Analysis_Ready_DS_jobs.csv")
 
 import streamlit.components.v1 as components
-st.markdown(
-    """
-    <script src="https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us" async></script>
-    """,
-    unsafe_allow_html=True
-)
+import streamlit as st
+import streamlit.components.v1 as components
 
+# Use a height of 0 so it doesn't create a blank space in your app
+# The script will still execute and create the floating bubble
+components.html(
+    """
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    """,
+    height=0,
+    width=0,
+)
 
 
 
