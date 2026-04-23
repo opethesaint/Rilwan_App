3afc506e4f9041cf3e2729a95831be85acb9b1ae
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 16:36:46 +0100

    Adjust TAWK_SCRIPT height in Streamlit app

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -35,7 +35,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 </script>
 """
 
-components.html(TAWK_SCRIPT, height=0, width=0)
+components.html(TAWK_SCRIPT, height=100)
 
 import streamlit as st
 components.html("""
