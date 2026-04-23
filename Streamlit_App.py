aaa8a43ba8ee90394b153fd90966dc3afed871b3
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-23 08:30:02 +0100

    Replace chat widget with Tawk.to integration

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -34,33 +34,30 @@ import streamlit as st
 import streamlit as st
 
 # Floating style container
-st.markdown("""
-    <div style="position: fixed; bottom: 50px; right: 20px; z-index: 1000;">
-        <!-- You can place buttons, links, or icons here -->
-    </div>
-""", unsafe_allow_html=True)
+import streamlit as st
+import streamlit.components.v1 as components
 
-# Tawk.to chat widget
-chat_widget = """
+tawk_widget = """
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+
+(function () {
+    var s1 = document.createElement("script");
+    var s0 = document.getElementsByTagName("script")[0];
+
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/69e9b89cb84bb21c2c7155f8/1jmsfi8us';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+
+    s0.parentNode.insertBefore(s1, s0);
 })();
 </script>
 <!--End of Tawk.to Script-->
 """
 
-# Embed the widget in Streamlit
-st.components.v1.html(chat_widget, height=600)
-
-
+components.html(tawk_widget, height=0)
 
 import streamlit as st
 
