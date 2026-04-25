710f9a4e0c3f3f87f390ab69b194ee778898c0ed
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-25 17:50:58 +0100

    Update Instagram link in Streamlit app

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -683,7 +683,7 @@ social_footer = """
         <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" width="40">
     </a>
 
-    <a href="https://instagram.com/yourusername" target="_blank" style="margin:15px;">
+    <a href="https://instagram.com/opethesaint/" target="_blank" style="margin:15px;">
         <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" width="40">
     </a>
 
