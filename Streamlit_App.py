e5b64dcaa04fa6504fc01a30850d806137f40c3e
opethesaint <52231356+opethesaint@users.noreply.github.com>
2026-04-27 13:36:28 +0100

    Load Firebase credentials from JSON string

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -33,17 +33,17 @@ components.html(CLARITY_CODE, height=0)
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials
+import json
 
 if not firebase_admin._apps:
 
-    cred = credentials.Certificate(
-        st.secrets["firebase"]["service_account"]
-    )
+    cred_json = json.loads(st.secrets["firebase"]["service_account"])
+
+    cred = credentials.Certificate(cred_json)
 
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://YOUR_PROJECT.firebaseio.com/"
     })
-
 
 
 
